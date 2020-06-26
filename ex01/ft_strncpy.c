a61699a747112e1f618e6627aee9f0bc9373a234
Sbusiso Mnisi <smnisi@c4r12s3.wethinkcode.co.za>
2020-06-26 13:55:02 +0200

    done

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+   char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	if (i < n && src[i] == '\0')
+	{
+		while (dest[i] != '\0')
+		{
+			dest[i] = '\0';
+			++i;
+		}
+	}
+	return (dest);
+}
